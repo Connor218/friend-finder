e77609cc1e650f48e2c53b2a97251ebbc0957b17
Admin <admin@Admins-MacBook-Pro.local>
2018-06-04 23:45:27 -0700

    update survey

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = {
+    "name":"Becky",
+    "photo":"http://cdn2.stylecraze.com/wp-content/uploads/2013/10/2.-Anushka-Sharma_1.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  
